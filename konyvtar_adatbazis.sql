f4c985dc9e13991fa97836acf78a20b462a0e5e0
Jaksa Márkó <jaksa.marko@diak.szbi-pg.hu>
2025-11-20 14:18:33 +0100

    Update konyvtar_adatbazis.sql

--- konyvtar_adatbazis.sql
+++ konyvtar_adatbazis.sql
@@ -1,4 +1,26 @@
--- 2. feladat -- 
+-- 2. feladat 
 CREATE DATABASE konyvtaradatbazis
     CHARACTER SET utf8mb4 
     COLLATE utf8mb4_hungarian_ci;
+
+    -- 3. feladat 
+USE konyvtaradatbazis;
+    -- 4. feladat 
+CREATE TABLE konyvek (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    cim VARCHAR(200) NOT NULL UNIQUE,
+    szerzo VARCHAR(120),
+    kiadasi_ev YEAR, 
+    oldalszam INTEGER CHECK (BETWEEN 20 AND 2000),
+    kategoria ENUM("fantasy", "sci-fi", "disztópia", "történelem")
+    ertekeles FLOAT DEFAULT 5.0 
+);
+    -- 5. feladat
+CREATE TABLE olvasok (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    nev VARCHAR(100) NOT NULL UNIQUE,
+    szuletesi_datum DATE,
+    varos VARCHAR(50) DEFAULT(Budapest),
+    aktiv DEFAULT(igen),
+    regisztracios_idopontja 
+);
